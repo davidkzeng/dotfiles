f9601e1f0b0cfd3a805ef18477d629714a449e4d
David Zeng <davidzeng.42@gmail.com>
2020-04-10 14:24:03 -0400

    add gitconfig, change setup

--- sh/link_files.sh
+++ sh/link_files.sh
@@ -1,7 +1,10 @@
 dotfiles_dir=$(dirname $(dirname $(realpath "$0")))
 
-to_link=(tmux.conf nvimrc bashrc.common)
+to_link=(tmux.conf nvimrc bashrc.common gitconfig)
 for file in "${to_link[@]}"; do
     ln -sf "$dotfiles_dir/$file" "$HOME/.$file"
 done
+
+mkdir "$HOME/bin"
+ln -sf "$dotfiles_dir/bin" "$HOME/custom-bin"
 
