44ef816abc7a267a6c7897cd8d90f5cbfa8abb89
David Zeng <davidzeng.42@gmail.com>
2020-07-22 20:53:16 -0400

    reorg

--- install/sh/link_files.sh
+++ install/sh/link_files.sh
@@ -1,6 +1,7 @@
 echo ">>> Started link_files.sh"
 
 dotfiles_dir=$1
+home_mirror="${dotfiles_dir}/mirror"
 
 function safe_link() {
     target=$1
@@ -11,6 +12,10 @@ function safe_link() {
     elif [[ ! -e "$target" ]]; then
         echo "$target does not exist... ignoring"
     else
+        if [[ -L "$link" ]]; then
+            echo "Removing broken symlink $link"
+            rm $link
+        fi
         echo "Added symlink $link -> $target"
         ln -s "$target" "$link"
     fi
@@ -20,13 +25,15 @@ safe_link "$dotfiles_dir" "$HOME/.dotfiles"
 
 to_link=(tmux.conf nvimrc gitconfig gitignore_global bash_aliases bash_functions bashrc_base)
 for file in "${to_link[@]}"; do
-    safe_link "$dotfiles_dir/$file" "$HOME/.$file"
+    safe_link "$home_mirror/$file" "$HOME/.$file"
 done
 
-safe_link "$dotfiles_dir/apps/vim/coc/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
-safe_link "$dotfiles_dir/apps/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
+to_link_config=(config/nvim/init.vim config/nvim/coc-settings.json config/Code/User/settings.json)
+for file in "${to_link_config[@]}"; do
+    safe_link "$home_mirror/$file" "$HOME/.$file"
+done
 
 mkdir -p "$HOME/bin"
-safe_link "$dotfiles_dir/bin" "$HOME/bin/custom_bin"
+safe_link "$home_mirror/bin/custom_bin" "$HOME/bin/custom_bin"
 
 echo "<<< Completed link_files.sh"
