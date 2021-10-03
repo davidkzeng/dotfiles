833a840c9bd95b0a1cc6d5b72b4d86b01ef2b3f1
David Zeng <davidzeng.42@gmail.com>
2021-10-03 18:21:24 -0400

    minor

--- sh/init_kde.sh
+++ sh/init_kde.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt install latte-dock
+timedatectl set-local-rtc 1 # fix windows dualboot time issues
+
 
 # TODO Manually because I haven't quite figured out the dotfiles way
 # ctrl + space -> KRunner
